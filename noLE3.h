b154b2bf086a37752ffe351d659c353dcaf41f75
elis <elisionara@alu.ufc.br>
2019-09-28 12:11:36 -0300

    Adding main file

---
+++ noLE3.h
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct no{
+    int elemento;//INFORMAÇÃO ARMAZENADA
+    struct no *prox;//PONTEIRO PARA O PRÓXIMO ELEMENTO CONTENDO UMA INFO + UM PONTEIRO
+
+};
+
+typedef struct no NO3;
+
+
+
